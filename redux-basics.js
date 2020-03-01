d3860a60556e8a2cb5bbd5c2b9666fced0425b5f
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-03-01 21:25:24 +0600

    add store.dispatch & store.subscribe

--- redux-basics.js
+++ redux-basics.js
@@ -6,10 +6,36 @@ const initalState = {
 };
 //Create a Reducer
 const rootReducer = (state = initalState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    };
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    };
+  }
   return state;
 };
 
 //Create a Store
 const store = createStore(rootReducer);
+
+//Create a Subscribe
+store.subscribe(() => {
+  console.log('SubScribe', store.getState());
+});
+
+//Store Dispatch
+store.dispatch({
+  type: 'INC_COUNTER'
+});
+store.dispatch({
+  type: 'ADD_COUNTER',
+  value: 10
+});
 
 console.log(store.getState());
